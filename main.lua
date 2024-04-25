1fe6aaca03c4001146c074fbe143b6b5fec64de9
Guilherme China <echina725@gmail.com>
2024-04-25 08:48:07 -0300

    understanding lua function parameters

--- main.lua
+++ main.lua
@@ -4,3 +4,30 @@ vim.api.nvim_create_autocmd("FileType", {
         vim.schedule(function() print("Lua file added") end)
     end
 })
+
+local humanReadableTable = vim.inspect({ favoriteDog = "Churros" })
+print(humanReadableTable)
+
+
+local fooX = function(a, b)
+    print("FooX: " .. tostring(a) .. " | " .. tostring(b))
+end
+
+local fooY = function(opts)
+    print("FooY: " .. tostring(opts.foo) .. " | " .. tostring(opts.name))
+end
+
+local fooZ = function(text)
+    print("FooZ: " .. tostring(text))
+end
+
+fooX("churros", "CHURROS")
+fooX("churros")
+
+-- the parenthesis can be ommtided if the function takes one string parameter, or a literal table
+fooY({ foo = true, name = "Churros" })
+fooY { foo = true, name = "Churros" }
+fooY { name = "Churros" }
+
+fooZ("ChurrosZ")
+fooZ "ChurrosZ"
