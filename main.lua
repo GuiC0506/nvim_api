3610956f7eabae8d6c756a18144c6a99cace4c1a
Guilherme China <echina725@gmail.com>
2024-04-28 16:19:28 -0300

    understanding lua module and plugins

--- main.lua
+++ main.lua
@@ -4,9 +4,8 @@ local mymodule = require("my-module")
 --[[ _G.test() ]]
 vim.api.nvim_command("echo \"Hello world\"")
 vim.cmd("echo \"hello world\"")
-vim.cmd [[ 
+vim.cmd [[
     echo "Hello world"
 ]]
 
 print(_VERSION)
-
