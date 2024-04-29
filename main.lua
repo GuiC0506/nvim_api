8dfa398393b90c03b85dbe969a74870345c90c72
Guilherme China <echina725@gmail.com>
2024-04-29 07:38:47 -0300

    understanding lua tables

--- main.lua
+++ main.lua
@@ -9,3 +9,28 @@ vim.cmd [[
 ]]
 
 print(_VERSION)
+
+-- pairs: iterates over string and numeric keys in a table. Order not garanteed
+-- ipairs: iterates only over numeric keys in a table or over an array. Order garanteed
+
+local table = {
+    ["Churros"] = function()
+        print("Churros")
+    end,
+    [1] = "Churros"
+}
+
+print("=================")
+for key, value in pairs(table) do
+    print(key, value)
+end
+
+print("=================")
+for x, y in pairs({ x = 5, y = 6 }) do
+    print(x, y)
+end
+
+print("=================")
+for i, value in ipairs { 1, 2, 3 } do
+    print(i, value * 2)
+end
